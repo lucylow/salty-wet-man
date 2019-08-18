e01d3b547f3d1d5ef26b048369235b2b081a6f4d
^˵◕ω◕˵^ <low.lucyy@gmail.com>
2019-08-18 15:26:53 -0400

    cleaned up js files

---
+++ js/main.js
@@ -0,0 +1,173 @@
+// REFERENCE: https://github.com/fchollet/deep-learning-with-python-notebooks/blob/master/5.4-visualizing-what-convnets-learn.ipynb
+
+const argparse = require('argparse');
+const fs = require('fs');
+const path = require('path');
+const shelljs = require('shelljs');
+const tf = require('@tensorflow/tfjs');
+
+const cam = require('./cam');
+const imagenetClasses = require('./imagenet_classes');
+const filters = require('./filters');
+const utils = require('./utils');
+
+function parseArguments() {
+  const parser =
+      new argparse.ArgumentParser({description: 'Visualize convnet'});
+  parser.addArgument('modelJsonUrl', {
+    type: 'string',
+    help: 'URL to model JSON. Can be a file://, http://, or https:// URL'
+  });
+  parser.addArgument('convLayerNames', {
+    type: 'string',
+    help: 'Names of the conv2d layers to visualize, separated by commas ' +
+        'e.g., (block1_conv1,block2_conv1,block3_conv1,block4_conv1)'
+  });
+  parser.addArgument('--inputImage', {
+    type: 'string',
+    defaultValue: '',
+    help: 'Path to the NSFW input image. If specified, will compute the internal' +
+        'activations of the specified convolutional layers. If not specified, ' +
+        'will compute the maximally-activating input images using gradient ascent.'
+  });
+  parser.addArgument('--outputDir', {
+    type: 'string',
+    defaultValue: 'dist/filters',
+    help: 'Output directory to which the image files and the manifest will ' +
+        'be written'
+  });
+  parser.addArgument('--filters', {
+    type: 'int',
+    defaultValue: 64,
+    help: 'Number of filters to visualize for each conv2d layer'
+  });
+  parser.addArgument('--iterations', {
+    type: 'int',
+    defaultValue: 80,
+    help: 'Number of iterations to use for gradient ascent'
+  });
+  parser.addArgument(
+      '--gpu',
+      {action: 'storeTrue', help: 'Use tfjs-node-gpu (required CUDA GPU).'});
+  return parser.parseArgs();
+}
+
+ // Calcuate and save the maximally-activating input images for a covn2d layer.
+
+async function writeConvLayerFilters(
+    model, layerName, numFilters, iterations, outputDir) {
+  const filePaths = [];
+  const maxFilters = model.getLayer(layerName).getWeights()[0].shape[3];
+  if (numFilters > maxFilters) {
+    numFilters = maxFilters;
+  }
+  for (let i = 0; i < numFilters; ++i) {
+    console.log(
+        `Processing layer ${layerName}, filter ${i + 1} of ${numFilters}`);
+    const imageTensor =
+        filters.inputGradientAscent(model, layerName, i, iterations);
+    const outputFilePath = path.join(outputDir, `${layerName}_${i + 1}.png`);
+    filePaths.push(outputFilePath);
+    await utils.writeImageTensorToFile(imageTensor, outputFilePath);
+    imageTensor.dispose();
+    console.log(`  --> ${outputFilePath}`);
+  }
+  return filePaths;
+}
+
+async function run() {
+  const args = parseArguments();
+  if (args.gpu) {
+    // Use GPU bindings.
+    require('@tensorflow/tfjs-node-gpu');
+  } else {
+    // Use CPU bindings.
+    require('@tensorflow/tfjs-node');
+  }
+
+  console.log('Loading model...');
+  if (args.modelJsonUrl.indexOf('http://') === -1 &&
+      args.modelJsonUrl.indexOf('https://') === -1 &&
+      args.modelJsonUrl.indexOf('file://') === -1) {
+    args.modelJsonUrl = `file://${args.modelJsonUrl}`;
+  }
+  const model = await tf.loadLayersModel(args.modelJsonUrl);
+  console.log('Model loading complete.');
+
+  if (!fs.existsSync(args.outputDir)) {
+    shelljs.mkdir('-p', args.outputDir);
+  }
+
+  if (args.inputImage != null && args.inputImage !== '') {
+    // Compute  internal activations of the conv layers' outputs.
+    const imageHeight = model.inputs[0].shape[1];
+    const imageWidth = model.inputs[0].shape[2];
+    const x = await utils.readImageTensorFromFile(
+        args.inputImage, imageHeight, imageWidth);
+    const layerNames = args.convLayerNames.split(',');
+    const {modelOutput, layerName2FilePaths, layerName2ImageDims} =
+        await filters.writeInternalActivationAndGetOutput(
+            model, layerNames, x, args.filters, args.outputDir);
+
+    // Calculate internal activations and final output of the model.
+    const topNum = 10;
+    const {values: topKVals, indices: topKIndices} =
+        tf.topk(modelOutput, topNum);
+    const probScores = Array.from(await topKVals.data());
+    const indices = Array.from(await topKIndices.data());
+    const classNames =
+        indices.map(index => imagenetClasses.IMAGENET_CLASSES[index]);
+
+    console.log(`Top-${topNum} classes:`);
+    for (let i = 0; i < topNum; ++i) {
+      console.log(
+          `  ${classNames[i]} (index=${indices[i]}): ` +
+          `${probScores[i].toFixed(4)}`);
+    }
+
+    // Save original NSFW input image and the top-10 classification results.
+    const origImagePath =
+        path.join(args.outputDir, path.basename(args.inputImage));
+    shelljs.cp(args.inputImage, origImagePath);
+
+    // Calculate Grad-CAM heatmap.
+    const xWithCAMOverlay = cam.gradClassActivationMap(model, indices[0], x);
+    const camImagePath = path.join(args.outputDir, 'cam.png');
+    await utils.writeImageTensorToFile(xWithCAMOverlay, camImagePath);
+    console.log(`Written CAM-overlaid image to: ${camImagePath}`);
+
+    // Create manifest and write it to disk.
+    const manifest = {
+      indices,
+      origImagePath,
+      classNames,
+      probScores,
+      layerName2FilePaths,
+      layerName2ImageDims,
+      camImagePath,
+      topIndex: indices[0],
+      topProb: probScores[0],
+      topClass: classNames[0]
+    };
+    const manifestPath = path.join(args.outputDir, 'activation-manifest.json');
+    fs.writeFileSync(manifestPath, JSON.stringify(manifest));
+  } else {
+    // Calculate the maximally-activating input images for the conv layers
+    const layerNames = args.convLayerNames.split(',');
+    const manifest = {layers: []};
+    for (let i = 0; i < layerNames.length; ++i) {
+      const layerName = layerNames[i];
+      console.log(
+          `\n=== Processing layer ${i + 1} of ${layerNames.length}: ` +
+          `${layerName} ===`);
+      const filePaths = await writeConvLayerFilters(
+          model, layerName, args.filters, args.iterations, args.outputDir);
+      manifest.layers.push({layerName, filePaths});
+    }
+    // Write manifest to file.
+    const manifestPath = path.join(args.outputDir, 'filters-manifest.json');
+    fs.writeFileSync(manifestPath, JSON.stringify(manifest));
+  }
+};
+
+run();
